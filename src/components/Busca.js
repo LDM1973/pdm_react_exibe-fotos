1912880012cea6a2cda0fc527e45273695addfbb
unknown <aluno@L4M02.IPIRANGA.FATEC>
2022-09-30 16:19:08 -0300

    feat(busca, layout, chave pexels): fim da pagina 22 da apostila 07

--- src/components/Busca.js
+++ src/components/Busca.js
@@ -3,33 +3,49 @@ import { InputText } from 'primereact/inputtext'
 import { Button } from 'primereact/button'
 
 export default class Busca extends Component {
+
+    constructor(props){
+        super(props)
+    }
+
+
     state = {
         termoDeBusca: ''
     }
 
     onTermoAlterado = (evento) => {
         console.log(evento.target.value)
+        this.setState({termoDeBusca: evento.target.value})
     }
+
+    onFormSubmit = (event) => {
+        event.preventDefault()
+        this.props.onBuscaRealizada(this.state.temoDeBusca)
+    }
+
     render() {
     return (
-        <div className='flex flex-column'>
-            <span className="p-input-icon-left w-full">
-                <i className="pi pi-search" />
-                <InputText
-                    className='w-full'
-                    onChange={this.onTermoAlterado}
-                    placeholder={this.props.dica}
+        <form onSubmit={this.onFormSubmit}>
+            <div className='flex flex-column'>
+                <span className="p-input-icon-left w-full">
+                    <i className="pi pi-search" />
+                    <InputText
+                    value={this.state.termoDeBusca}
+                        className='w-full'
+                        onChange={this.onTermoAlterado}
+                        placeholder={this.props.dica}
+                    />
+                </span>
+                <Button 
+                    label='OK'
+                    className='p-button mt-2'
                 />
-            </span>
-            <Button 
-                label='OK'
-                className='p-button mt-2'
-            />
-        </div>
+            </div>
+        </form>
         )
     }
 }
 
 Busca.defaultProps = {
-    dica: "Digite algo..."
+    dica: "Digite algo que deseja ver"
 }
